96ce059d5da5b0aa132434568b0b264b6385c088
Yauheni Stetukevich <y.stsetsiukevihc@itechart-group.com>
2020-09-23 20:16:49 +0300

    Some changes in project VS

--- Git_VS/Git_VS/Program.cs
+++ Git_VS/Git_VS/Program.cs
@@ -11,6 +11,7 @@ namespace Git_VS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Some changes");
             Console.ReadLine();
         }
     }
